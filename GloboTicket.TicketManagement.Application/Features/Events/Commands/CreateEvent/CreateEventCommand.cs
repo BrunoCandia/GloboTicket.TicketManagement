5a67feaf3d7cd20cade95ab6f9f94fad43ee50ca
Bruno Candia <bruno.candia@endava.com>
2025-02-04 10:41:46 -0300

    Change to DateTimeOffset done.

--- GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommand.cs
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommand.cs
@@ -7,14 +7,14 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
         public string Name { get; set; } = string.Empty;
         public int Price { get; set; }
         public string Artist { get; set; } = string.Empty;
-        public DateTime Date { get; set; }
+        public DateTimeOffset Date { get; set; }
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
         public Guid CategoryId { get; set; }
 
         public override string ToString()
         {
-            return $"Event name: {Name}; Price: {Price}; By: {Artist}; On: {Date.ToShortDateString()}; Description: {Description}";
+            return $"Event name: {Name}; Price: {Price}; By: {Artist}; On: {Date.ToString()}; Description: {Description}";
         }
     }
 }
